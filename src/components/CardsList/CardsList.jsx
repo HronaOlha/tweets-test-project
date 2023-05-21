cb8b144f406f8f95b830ef974553702ea98f3b84
Грона Оля <grona.hell@gmail.com>
2023-05-21 03:01:51 +0300

    add: pagination

--- src/components/CardsList/CardsList.jsx
+++ src/components/CardsList/CardsList.jsx
@@ -1,33 +1,54 @@
 import { useEffect, useState } from "react";
 import Card from "../Card/Card";
-import { List } from "./CardsList.styled";
+import { List, ListContainer, Button } from "./CardsList.styled";
 
 function CardsList() {
   const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    fetch(`https://6453dfb6c18adbbdfeaa041f.mockapi.io/tweets/users`)
-      .then((res) => res.json())
-      .then((results) => {
-        setUsers(results);
-      });
-  }, []);
+    if (page === 1) {
+      fetch(
+        `https://6453dfb6c18adbbdfeaa041f.mockapi.io/tweets/users?page=1&limit=3`
+      )
+        .then((res) => res.json())
+        .then((results) => {
+          setUsers(results);
+        });
+    }
+    if (page > 1) {
+      fetch(
+        `https://6453dfb6c18adbbdfeaa041f.mockapi.io/tweets/users?page=${page}&limit=3`
+      )
+        .then((res) => res.json())
+        .then((results) => {
+          setUsers((prev) => [...prev, ...results]);
+        });
+    }
+  }, [page]);
+
+  function onLoadMore() {
+    setPage(page + 1);
+  }
 
   return (
-    <List>
-      {users &&
-        users.map(({ id, tweets, avatar, followers, user }) => (
-          <li key={id}>
-            <Card
-              id={id}
-              tweets={tweets}
-              avatar={avatar}
-              followers={followers}
-              alt={user}
-            />
-          </li>
-        ))}
-    </List>
+    <ListContainer>
+      <List>
+        {users &&
+          users.map(({ id, tweets, avatar, followers, user }) => (
+            <li key={id}>
+              <Card
+                id={id}
+                tweets={tweets}
+                avatar={avatar}
+                followers={followers}
+                alt={user}
+              />
+            </li>
+          ))}
+      </List>
+      <Button onClick={onLoadMore}>Load More</Button>
+    </ListContainer>
   );
 }
 
--- src/components/CardsList/CardsList.styled.js
+++ src/components/CardsList/CardsList.styled.js
@@ -8,12 +8,27 @@ export const List = styled.ul`
   align-items: center;
 `;
 
-// export const ListContainer = styled.div`
-//   margin: auto;
-//   display: flex;
-//   justify-content: flex-end;
-//   flex-wrap: wrap;
-//   flex-direction: row;
-//   place-items: center;
-//   gap: 24px;
-// `;
+export const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  place-items: center;
+  gap: 24px;
+`;
+
+export const Button = styled.button`
+  width: 196px;
+  border: none;
+  cursor: pointer;
+  padding: 14px 0;
+  box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  color: #373737;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 1.22;
+  text-transform: uppercase;
+
+  &:hover {
+    background-color: #595959;
+  }
+`;
